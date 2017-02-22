aadf1484347e990d77c033e49a88bd017356334d
Vladimir Alekseichenko <vova@vova.me>
2017-02-22 10:15:19 +0100

    set token for noteobooks

--- start_script.sh
+++ start_script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-jupyter notebook --notebook-dir=/opt --ip=* --port=8888 --no-browser &> /dev/null &
-jupyter lab --notebook-dir=/opt --ip=* --port=8889 --no-browser &> /dev/null &
+jupyter notebook --notebook-dir=/opt/notebooks --ip=* --port=8888 --no-browser  --NotebookApp.token='dataworkshop' &> /dev/null &
+jupyter lab --notebook-dir=/opt/notebooks --ip=* --port=8889 --no-browser  --NotebookApp.token='dataworkshop' &> /dev/null &
 python /opt/prerequisite/run.py &
 /bin/bash
+
+
